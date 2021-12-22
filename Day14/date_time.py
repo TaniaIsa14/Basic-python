62f2660660a9a8ee934a22dc6928ad90b0e3d401
Tania Isa <34392109+TaniaIsa14@users.noreply.github.com>
2021-12-22 19:29:55 +0600

    Day 14

---
+++ Day14/date_time.py
@@ -0,0 +1,59 @@
+'''
+Date & Time
+--------------
+ date class
+ time class
+ datetime class
+ timedelta class
+'''
+
+#from datetime import datetime,date
+from datetime import *
+dtobj=datetime.now()
+print(dtobj)
+
+dtobj2=date.today()
+print(dtobj2)
+
+print(dtobj.year)
+print(dtobj.month)
+print(dtobj.day)
+
+print(dtobj.hour)
+print(dtobj.minute)
+print(dtobj.second)
+print(dtobj.microsecond)
+
+print(dtobj.strftime('week Day short : %a'))
+print(dtobj.strftime('week Day long : %A'))
+print(dtobj.strftime('week Day number : %w'))
+
+print(dtobj.strftime('Month short : %b'))
+print(dtobj.strftime('Month long : %B'))
+print(dtobj.strftime('Month number : %m'))
+
+
+print(dtobj.strftime('Day of month : %d'))
+print(dtobj.strftime('Year without century : %y'))
+print(dtobj.strftime('Year with century : %Y'))
+
+print(dtobj.strftime('Full time 0-23 : %T'))
+print(dtobj.strftime('Full Date 0-23 : %D'))
+
+
+print(dtobj.strftime('Hour 0-23 : %H'))
+print(dtobj.strftime('Hour 0-12 : %I'))
+print(dtobj.strftime('AM:PM : %p'))
+print(dtobj.strftime('Minute : %M'))
+print(dtobj.strftime('Second : %S'))
+print(dtobj.strftime('Microsecond : %f'))
+
+
+print(dtobj.strftime('Dispaly customer format: '))
+print('-'*40)
+# 21-1-2021
+print(dtobj.strftime('%d-%m-%Y'))
+print(dtobj.strftime('%d.%m.%Y'))
+print(dtobj.strftime('%A,%d-%m-%Y'))
+print(dtobj.strftime('%I:%M:%S %p'))
+print(dtobj.strftime('%H:%M:%S '))
